f63f852d78bc04ead45b3f85f662fa851a08a3be
g-yonchev <georgi.d.yonchev@gmail.com>
2025-11-25 21:44:36 +0100

    Add LandmarkRow

---
+++ SwiftUITutorial/Views/LandmarkRow.swift
@@ -0,0 +1,25 @@
+//
+//  LandmarkRow.swift
+//  SwiftUITutorial
+//
+//  Created by Zhoro on 11/25/25.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+//#Preview {
+//    LandmarkRow(landmark: ModelData().landmarks[1])
+//}
