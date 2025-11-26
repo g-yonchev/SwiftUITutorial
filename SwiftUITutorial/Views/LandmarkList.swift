01c2bcda59fbd66dbd6c3ab6b7b29b3289f12c04
g-yonchev <georgi.d.yonchev@gmail.com>
2025-11-26 10:14:35 +0100

    Wrap landmarks into a navigation split view and provide a like for each landmark detail view

--- SwiftUITutorial/Views/LandmarkList.swift
+++ SwiftUITutorial/Views/LandmarkList.swift
@@ -9,8 +9,19 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List(landmarks) { landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationSplitView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail()
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        } detail: {
+            ContentUnavailableView {
+                Label("Select a Landmark", systemImage: "mountain.2.fill")
+            }
         }
     }
 }
