5c1a19a4bf98201f428cd319050dfa3fb8c82c9a
g-yonchev <georgi.d.yonchev@gmail.com>
2025-11-26 16:34:09 +0100

    Add badges and hikes into profile summary

--- Landmarks/Views/Profiles/ProfileSummary.swift
+++ Landmarks/Views/Profiles/ProfileSummary.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 struct ProfileSummary: View {
+    @Environment(ModelData.self) var modelData
     var profile: Profile
+
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 10) {
@@ -18,7 +20,40 @@ struct ProfileSummary: View {
 
                 Text("Notifications: \(profile.prefersNotifications ? "On": "Off" )")
                 Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
-                Text("Goal Date: \(Text(profile.goalDate, style: .date))") 
+                Text("Goal Date: \(Text(profile.goalDate, style: .date))")
+
+                Divider()
+
+                VStack(alignment: .leading) {
+                    Text("Earned Badges")
+                        .font(.headline)
+                    
+                    ScrollView(.horizontal) {
+                        HStack {
+                            HikeBadge(name: "Firts Hike")
+                            HikeBadge(name: "Earth Day")
+                                .hueRotation(Angle(degrees: 45))
+                            HikeBadge(name: "Keep Strong")
+                                .hueRotation(Angle(degrees: 90))
+                            HikeBadge(name: "Tenth Hike")
+                                .hueRotation(Angle(degrees: 180))
+                                .grayscale(0.5)
+                        }
+                        .padding(.bottom)
+                    }
+                }
+
+                Divider()
+
+                
+                VStack(alignment: .leading) {
+                    Text("Recent Hikes")
+                        .font(.headline)
+
+                    HikeView(hike: modelData.hikes[0])
+                }
+                
+
             }
         }
     }
@@ -26,4 +61,5 @@ struct ProfileSummary: View {
 
 #Preview {
     ProfileSummary(profile: Profile.default)
+        .environment(ModelData())
 }
