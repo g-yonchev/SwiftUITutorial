5e0936d1759937fe2a52ab918b8c34a47e163dd6
g-yonchev <georgi.d.yonchev@gmail.com>
2025-11-26 10:26:56 +0100

    Display details for each landmark

--- SwiftUITutorial/Views/LandmarkDetail.swift
+++ SwiftUITutorial/Views/LandmarkDetail.swift
@@ -8,20 +8,21 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    var landmark: Landmark
     var body: some View {
-        VStack {
-            MapView()
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
                 .frame(height: 300)
-            CircleImage()
+            CircleImage(image: landmark.image)
                 .offset(y: -130)
                 .padding(.bottom, -130)
             VStack(alignment: .leading) {
-                Text("Turtle Rock")
+                Text(landmark.name)
                     .font(.title)
                 HStack {
-                    Text("Joshua Tree National Park")
+                    Text(landmark.park)
                     Spacer()
-                    Text("California")
+                    Text(landmark.state)
                 }
                 .font(.subheadline)
                 .foregroundStyle(.secondary)
@@ -29,8 +30,8 @@ struct LandmarkDetail: View {
 
                 Divider()
 
-                Text("About Turtle Rock").font(.title2)
-                Text("Descriptive text goes here.")
+                Text("About \(landmark.name)").font(.title2)
+                Text(landmark.description)
             }
             .padding()
 
@@ -40,5 +41,5 @@ struct LandmarkDetail: View {
 }
 
 #Preview {
-    LandmarkDetail()
+    LandmarkDetail(landmark: landmarks[1])
 }
